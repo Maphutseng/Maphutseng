ee2e977131b9d7f6eb614e53a4090f57ad034f3b
Maphutseng <mphetholemaphutseng@gmail.com>
2025-03-11 02:54:06 -0700

    Delete assignment1 directory

--- assignment1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignment1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.assignment1 to javafx.fxml;
-    exports com.example.assignment1;
-}
